27088bc6b89b38721c8acc797783ec5562e664e0
chandan_singh_ai <chandan.singh@stl.tech>
2024-09-12 16:09:48 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Commercial-WIP-Backend
-# Commercial-WIP-Backend
--- create_tables.py
+++
@@ -1,8 +0,0 @@
-# create_tables.py
-
-from app import app, db
-
-# Run the database creation inside the application context
-with app.app_context():
-    db.create_all()
-    print("Tables created successfully!")
